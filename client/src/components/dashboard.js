1fd90d582b49316511fb433791ccdd21b552f1ea
nastex21 <nasax2000@gmail.com>
2019-05-11 10:44:43 -0500

    CSS fixed
    +CSS was imported and fixed for the calendar.

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import '@fullcalendar/core/main.css';
+import '@fullcalendar/daygrid/main.css';
 
 class Dashboard extends Component {
 
