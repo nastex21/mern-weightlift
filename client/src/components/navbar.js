efd356e5ed52718d482f495f5f8d450851ffa591
nastex21 <nasax2000@gmail.com>
2019-05-13 11:57:57 -0500

    Changed "Weight Lifting Tracker" redirect
    +depends if you're logged in or not to where the Weight Lifting Tracker redirect in the navbar sends you.

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -47,33 +47,27 @@ class Navbar extends Component {
             return (
                 <div>
                     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-                        <div className="collapse navbar-collapse" id="navbarColor01" >
+                            <a class="navbar-brand" href={loggedIn ? "/dashboard" : "/"}>Weight Lifting Tracker</a>
+                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                            <div className="collapse navbar-collapse" id="navbarColor01" >
                             {loggedIn ? (
-                                <div className="collapse navbar-collapse" id="navbarColor01">
-                                    <ul className="navbar-nav mr-auto">
-                                        <li className="nav-item active">
-                                            <a className="nav-link" href="/">Weight Lifting Tracker<span className="sr-only">(current)</span></a>
-                                        </li>
-                                        <li className="nav-item active">
-                                            <a className="nav-link" href="/" onClick={this.logout}>Log Out<span className="sr-only">(current)</span></a>
-                                        </li>
-                                    </ul>
-                                </div>
+                                <ul className="navbar-nav mr-auto">
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="/" onClick={this.logout}>Log Out<span className="sr-only">(current)</span></a>
+                                    </li>
+                                </ul>
                             ) : (
 
-                                    <div className="collapse navbar-collapse" id="navbarColor01">
-                                        <ul className="navbar-nav mr-auto">
-                                            <li className="nav-item active">
-                                                <a className="nav-link" href="/">Weight Lifting Tracker<span className="sr-only">(current)</span></a>
-                                            </li>
-                                            <li className="nav-item">
-                                                <a className="nav-link" href="/login">Login</a>
-                                            </li>
-                                            <li className="nav-item">
-                                                <a className="nav-link" href="/signup">Register</a>
-                                            </li>
-                                        </ul>
-                                    </div>
+                                    <ul className="navbar-nav mr-auto">
+                                        <li className="nav-item">
+                                            <a className="nav-link" href="/login">Login</a>
+                                        </li>
+                                        <li className="nav-item">
+                                            <a className="nav-link" href="/signup">Register</a>
+                                        </li>
+                                    </ul>
                                 )}
                         </div>
                     </nav>
