2d22cc8bea5b9c38da6e309120f059faa735ee1d
nastex21 <nasax2000@gmail.com>
2019-05-22 15:53:55 -0500

    Added click even to calendar
    +Added the interaction plugin to add events to the calendar.
    +Added a bootstrap stylesheet to the calendar.
    
    *Need to add functionality to the click event (such as allowing the client to add an exercise, maybe through a modal but for now, leave as is)
    *Want to fill calendar with events in the next update.

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import FullCalendar from '@fullcalendar/react';
+import bootstrapPlugin from '@fullcalendar/bootstrap';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
 import '@fullcalendar/core/main.css';
 import '@fullcalendar/daygrid/main.css';
+import '@fullcalendar/bootstrap/main.css';
+
 
 class Dashboard extends Component {
+
+    test = (info) => {
+        alert('Clicked on: ' + info.dateStr)
+    }
 
     render() {
         const logs = this.props.logs;
         console.log(logs);
         return (
-            <FullCalendar defaultView="dayGridMonth" plugins={[ dayGridPlugin ]} />
+            <FullCalendar defaultView="dayGridMonth" plugins={[ dayGridPlugin, bootstrapPlugin, interactionPlugin ]} themeSystem='bootstrap' selectable="true"  dateClick={this.test} />
           )
 
     }
