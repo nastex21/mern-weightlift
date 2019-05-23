a7619f2a94e9a421761c2dae219cf2bafd9f1972
Tony <nasax2000@gmail.com>
2019-05-22 21:33:03 -0500

    Events now work but...
    +events now work but only an example. Need to format what's in the database to a useable format or maybe change the schema.

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -9,6 +9,18 @@ import '@fullcalendar/bootstrap/main.css';
 
 
 class Dashboard extends Component {
+    state = {
+        events: []
+    }
+
+    componentDidMount(){
+        const newEvents =  [{ title: 'event 1', date: '2019-05-01' },
+        { title: 'event 2', date: '2019-05-20' }];
+        this.setState ({
+            events: [...this.state.events, ...newEvents]
+        })        
+        console.log(this.state.events);
+    }
 
     test = (info) => {
         alert('Clicked on: ' + info.dateStr)
@@ -17,8 +29,9 @@ class Dashboard extends Component {
     render() {
         const logs = this.props.logs;
         console.log(logs);
+        console.log(this.state.events);
         return (
-            <FullCalendar defaultView="dayGridMonth" plugins={[ dayGridPlugin, bootstrapPlugin, interactionPlugin ]} themeSystem='bootstrap' selectable="true"  dateClick={this.test} />
+            <FullCalendar defaultView="dayGridMonth" plugins={[ dayGridPlugin, bootstrapPlugin, interactionPlugin ]} themeSystem='bootstrap' selectable="true"  dateClick={this.test}   events={this.state.events} />
           )
 
     }
