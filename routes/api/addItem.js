d7d799dad1324c7b81cfa7ac4c50fb0e29ff76a7
nastex21 <nasax2000@gmail.com>
2019-06-02 14:09:18 -0500

    Commit

--- routes/api/addItem.js
+++ routes/api/addItem.js
@@ -5,17 +5,12 @@ const Log = require("../../database/models/logs");
 const Collections = require("../../database/models/collections");
 
 router.post('/', (req, res) => {
-    console.log(req.body.id);
-    console.log(req.body.date);
-    console.log("addItem triggered");
     const arr = [];
     let newDate = new Date(req.body.date);
-    console.log(newDate);
     var y = newDate.getFullYear();
     var m = newDate.getMonth() + 1;
     var d = newDate.getDate();
     const nowDate = y + "-" + m + "-" + d;
-    console.log(nowDate);
     const exObj = [];
     req.body.collection.forEach(item => exObj.push(item));
     var update = {
@@ -23,20 +18,20 @@ router.post('/', (req, res) => {
         collections: [...exObj]
     }
 
-    User.findOneAndUpdate({"_id": req.body.id}, {$addToSet:{"logs": update}}, (err, data) => { 
-        if (err) {
-            console.log("500");
-            return res.status(500).send(err);
-        }
-        if (!data) {
-            console.log("404");
-            return res.status(404).end();
-        }
+User.findByIdAndUpdate(req.body.id, {$push: {"logs": update}}, (err, data) => {
+    if (err) {
+        console.log("500");
+        return res.status(500).send(err);
+    }
+    if (!data) {
+        console.log("404");
+        return res.status(404).end();
+    }
+    console.log("200");
+    console.log(data.logs);
+} )
 
-        return;
-        }
-    )
-})
 
+});
 
 module.exports = router;
