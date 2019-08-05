bda2f900b69819f3df11f0773d34c9d4414c97cc
nastex21 <nasax2000@gmail.com>
2019-08-04 21:01:09 -0500

    Commit

--- client/src/reducers/authenticationReducer.js
+++ client/src/reducers/authenticationReducer.js
@@ -1,13 +1,13 @@
 import { LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE, LOGOUT } from '../actions/types';
 
-let user = JSON.parse(localStorage.getItem('user'));
+var user = JSON.parse(localStorage.getItem('user'));
 console.log("authen reducer");
 console.log(user);
 const initialState = user ? { loggedIn: true, id: '', user } : {};
 
 export default function authentication(state = initialState, action) {
     console.log('action');
-    console.log(action);
+    console.log(user.data.id);
   
     switch (action.type) {
         case LOGIN_REQUEST:
@@ -17,7 +17,7 @@ export default function authentication(state = initialState, action) {
             };
         case LOGIN_SUCCESS:
             return {...state,
-                id: action.user,
+                id: user.data.id,
                 loggedIn: true,
             };
         case LOGIN_FAILURE:
