d1fdbdf3991afe99ec9c01b8736ba737c7974f2d
nastex21 <nasax2000@gmail.com>
2019-08-16 23:19:12 -0500

    Commit

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -109,7 +109,7 @@ class Dashboard extends Component {
 
 
     render() {
-        const { exercise, date, color } = this.state;
+        const { color } = this.state;
         console.log("this.props.modalisOpen");
         console.log(this.props);
 
@@ -117,9 +117,9 @@ class Dashboard extends Component {
             <div className="calendar-body">
                 <LeftPane date={this.state.oldDate} exercise={this.state.updatedInfo} filterButton={this.props.filterButton} />
                 <FullCalendar className="fcDiv bg-dark text-white" defaultView="dayGridMonth" timeZone='local' height="auto" displayEventTime="false" plugins={[dayGridPlugin, bootstrapPlugin, interactionPlugin]} themeSystem='bootstrap' selectable="true" dateClick={this.dateClickInfo} events={this.props.dataModifier.events} eventClick={this.toggle} />
-                <Modal isOpen={this.state.modal} toggle={this.toggle} size="lg" style={{ maxWidth: '1600px', width: '80%' }} color={this.state.color} onClosed={this.showErrorMsg}>
+                <Modal isOpen={this.state.modal} toggle={this.toggle} size="lg" style={{ maxWidth: '1600px', width: '80%' }} color={color} onClosed={this.showErrorMsg}>
                     <ModalHeader toggle={this.toggle}>
-                        <p className="exerciseTitle">{this.state.color == "#f0ad4e" ? "Exercise videos and/or classes" : this.state.color == "#d9534f" ? "Weightlifting Exercises" : this.state.color == "#0275d8" ? "Cardio Exercises" : this.state.color == "#5cb85c" ? "Bodyweight Exercises" : null}</p>
+                        <p className="exerciseTitle">{color == "#f0ad4e" ? "Exercise classes and/or videos" : color == "#d9534f" ? "Weightlifting Exercises" : color == "#0275d8" ? "Cardio Exercises" : color == "#5cb85c" ? "Bodyweight Exercises" : null}</p>
                         <p className="dateTitle">{this.props.dataModifier.dateText}</p>
                     </ModalHeader>
                     <ModalBody >
@@ -127,7 +127,7 @@ class Dashboard extends Component {
                             <button type="button" class="close" data-dismiss="alert" onClick={this.closeErr}>&times;</button> <span>{this.state.msg}</span>
                         </div>}
                         {this.state.modalVer == "add" ? <ModalTabs msgUpdate={this.showErrorMsg} color={color} /> : null}
-                        {this.state.modalVer == "edit" ? <ModalEditDel title={this.state.title} date={date} msgUpdate={this.showErrorMsg} exerciseArr={exercise} color={color} /> : null}
+                        {this.state.modalVer == "edit" ? <ModalEditDel title={this.state.title} msgUpdate={this.showErrorMsg} /> : null}
                     </ModalBody>
                 </Modal>
             </div>
