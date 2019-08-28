93b9c6058b3926ff8d24a34ce3324c12ef4d07d6
nastex21 <nasax2000@gmail.com>
2019-08-28 16:15:51 -0500

    Register works again.

--- routes/api/signup.js
+++ routes/api/signup.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const User = require('../../database/models/user');
-const passport = require('passport');
-const jwt = require('jsonwebtoken');
 const router = express.Router();
 
-router.post('/',
-  passport.authenticate('signup', { session: false }),
-  async (req, res, next) => {
-    res.json({ 
-      message : 'Signup successful',
-      user : req.user 
-    });
-  });
+router.post('/', (req, res) => {
+
+  const { username, password, logs } = req.body
+  // ADD VALIDATION
+  User.findOne({ username: username }, (err, user) => {
+    if (err) {
+      console.log('User.js post error: ', err)
+    } else if (user) {
+      res.json({
+        error: `Sorry, there's already a user with the username: ${username}`
+      })
+    } else {
+      const newUser = new User({
+        username: username,
+        password: password
+      })
+
+
+      newUser.save((err, savedUser) => {
+        if (err) return res.json(err)
+        res.json(savedUser)
+      })
+    }
+  })
+});
+
 module.exports = router;
