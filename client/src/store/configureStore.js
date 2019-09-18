ea8847b5e9fed6a94223b193d34bf354f4f1d91e
Tony <nasax2000@gmail.com>
2019-09-17 23:00:41 -0500

    Got rid of redux dev tools
    Heroku kept failing because of it.

--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -1,5 +1,4 @@
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from '../reducers';
 import thunk from "redux-thunk";
 
@@ -7,7 +6,7 @@ export default function configureStore(initialState) {
     const store = createStore(
       rootReducer,
       initialState,
-      composeWithDevTools(applyMiddleware(thunk))
+      applyMiddleware(thunk)
           );
   
     if (module.hot) {
